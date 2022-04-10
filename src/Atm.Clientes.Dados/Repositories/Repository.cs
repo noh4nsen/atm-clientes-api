8d3702fe77d102764d2f9a025632ff128d951691
noh4nsen <noh4nsen@gmail.com>
2022-04-10 18:52:26 -0300

    Correção timezone Brasileiro

--- src/Atm.Clientes.Dados/Repositories/Repository.cs
+++ src/Atm.Clientes.Dados/Repositories/Repository.cs
@@ -67,7 +67,7 @@ namespace Atm.Clientes.Dados.Repositories
         public void SetInsertData(Entity entity)
         {
             entity.Id = Guid.NewGuid();
-            entity.DataCadastro = DateTime.Now;
+            entity.DataCadastro = GetLocalTime();
             entity.DataAtualizacao = null;
         }
 
@@ -79,8 +79,13 @@ namespace Atm.Clientes.Dados.Repositories
 
         private T SetUpdateData(T entity)
         {
-            entity.DataAtualizacao = DateTime.Now;
+            entity.DataAtualizacao = GetLocalTime();
             return entity;
+        }
+
+        private static DateTime GetLocalTime()
+        {
+            return TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.FindSystemTimeZoneById("America/Sao_Paulo"));
         }
     }
 }
